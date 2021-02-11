5cf718d2471cea9df070169b761d45c5588b9635
laurendobratz00 <laurendobratz@icloud.com>
2021-02-11 19:53:04 +0000

    add comments

--- models/base_model.py
+++ models/base_model.py
@@ -14,15 +14,15 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """__init method
         """
-        if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-                elif key == "created_at" or key == "updated_at":
+        if len(kwargs) != 0: # as long as it's not empty
+            for key, value in kwargs.items(): # iterating through dictionary
+                 if key != "__class__": # class should not be added as attribute
+                    setattr(self, key, value) # setting attr for other keys
+                elif key == "created_at" or key == "updated_at": # convert to dt
                     time = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, time)
-        else:
-            self.id = str(uuid.uuid4())
+                    setattr(self, key, time) # sets attribute
+        else: # if kwargs is empty
+            self.id = str(uuid.uuid4()) # create id and created_at
             self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
@@ -31,22 +31,24 @@ class BaseModel:
         Return: [<class name>] (<self.id>) <self.__dict__>
         """
         return ("[BaseModel] ({}) {}".format(self.id, self.__dict__))
+# ^ printing id and dictionary in str format
 
     def save(self):
         """ save method:
             updates public instance attribute 'updated_at' w/ current datetime
         Args: self
         """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.now() # updates time to be current time
 
     def to_dict(self):
         """ to_dict method:
             returns a dictionary containing all keys/values of
             __dict__ of the instance
         """
-        my_dict = dict(self.__dict__)
+        my_dict = dict(self.__dict__) # make variable my_dict
         my_dict["__class__"] = str(type(self).__name__)
+# ^ adds class key with class name of the object
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
-
+# ^ convert keys to string objects at iso format
         return my_dict
