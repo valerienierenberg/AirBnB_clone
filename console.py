a391a41cd6b35e92e744e5673d6a76216a32fe1b
valerienierenberg <valerie.nierenberg@gmail.com>
2021-02-12 20:54:36 +0000

    Add console.py progress

--- console.py
+++ console.py
@@ -4,6 +4,7 @@
 
 import cmd
 import sys
+# from models import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -12,14 +13,39 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        """ Quit command to exit the program
+        """Quit command to exit the program
         """
         sys.exit(101)
 
     def do_EOF(self, arg):
-        """ EOF command to exit the program
+        """EOF command to exit the program
         """
+        self.non_interactive_mode()
         return True
+
+    @staticmethod
+    def non_interactive_mode():
+        """ Checks if interactive or noninteractive mode """
+        if sys.stdin.isatty() is False:
+            print("")
+
+    def emptyline(self):
+        """ an empty line + ENTER does not execute anything """
+        self.non_interactive_mode()
+        pass
+
+    def do_help(self, *args):
+        """ help method """
+        self.non_interactive_mode()
+        cmd.Cmd.do_help(self, *args)
+
+    def do_create(self, arg):
+        """create method - Creates a new instance of BaseModel,
+        saves it (to the JSON file) and prints the id. Ex: $ create BaseModel
+        """
+        myobj = BaseModel()
+        myobj.save()
+        print(myobj.id)
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
