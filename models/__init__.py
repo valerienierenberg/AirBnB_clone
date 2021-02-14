7c3727f2fc0c7eab7d8a7925f1b78768a5e71d4a
laurendobratz00 <laurendobratz@icloud.com>
2021-02-14 20:49:28 +0000

    fixed random letters that were added

--- models/__init__.py
+++ models/__init__.py
@@ -15,33 +15,3 @@ classes = {"BaseModel": BaseModel, "City": City, "Place": Place, "Amenity": Amen
 
 storage = file_storage.FileStorage()
 storage.reload()
-<<<<<<< HEAD
-
-"""
-base_model.py
-user.py
-state.py
-city.py
-amenity.py
-place.py
-review.py
-engine/
-    file_storage.py
-    __init__.py
-"""
-
-console.py
-models
-    __init__.py
-    amenity.py
-    base_model.py
-    city.py
-    place.py
-    review.py
-    state.py
-    user.py
-    engine
-        __init__.py
-        file_storage.py
-=======
->>>>>>> 4419a3545a78abca15d9eae785803e799aa6cd21
