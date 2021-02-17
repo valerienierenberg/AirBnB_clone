4bcf564a6f96345217ca07844494393b39907614
valerienierenberg <valerie.nierenberg@gmail.com>
2021-02-17 16:40:03 +0000

    Add console test file

---
+++ tests/test_console.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Unittests for console class and all its attributes and methods
+"""
+import unittest
+import pep8
+import json
+import sys
+import os
+from console import HBNBCommand
+
+class TestConsole(unittest.TestCase):
+    """ Tests Console Class """
+
+    def test_console(self):
+        """ Tests console method """
