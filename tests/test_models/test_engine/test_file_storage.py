96da902ad78aa2224152e8223022bbbdf5dac604
valerienierenberg <valerie.nierenberg@gmail.com>
2021-02-17 17:10:00 +0000

    Add file_storage method tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -8,12 +8,14 @@ import sys
 import os
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+from models import storage
+from os.path import exists
 
-class Test_Storage(unittest.TestCase):
-    """ Tests Test_Storage Class """
+class TestFileStorage(unittest.TestCase):
+    """ Tests File_Storage Class """
 
     def FileStorage(self):
-        """ tests for test_storage """
+        """ tests for file_storage """
         self.assertTrue(hasattr(FileStorage, "all"))
         self.assertTrue(hasattr(FileStorage, "new"))
         self.assertTrue(hasattr(FileStorage, "save"))
@@ -22,3 +24,29 @@ class Test_Storage(unittest.TestCase):
         self.assertTrue(FileStorage.new.__doc__)
         self.assertTrue(FileStorage.save.__doc__)
         self.assertTrue(FileStorage.reload.__doc__)
+
+    def test_all(self):
+        """ tests all method of file_storage """
+        myobj = BaseModel()
+        __objects = storage.all()
+
+    def test_new(self):
+        """ tests new method of file_storage """
+        myobj = BaseModel()
+        #  store myobj into __objects variable by <class name>.id using new()
+        __objects = storage.new(myobj)
+
+    def test_save(self):
+        """ tests save method of file_storage """
+        __file_path = "file.json"
+        storage.save()
+        #  check if file.json file was created by save method
+        self.assertTrue(exists(__file_path))
+
+    def test_reload(self):
+        """ tests reload method of file_storage """
+        myobj = BaseModel()
+        __objects = storage.all()
+        storage.reload()
+        __objects_reloaded = storage.all()
+        self.assertEqual(__objects, __objects_reloaded)
