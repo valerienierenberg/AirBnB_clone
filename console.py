1cb6bc6b77d7f58eea56faec3527e4532d480e12
laurendobratz00 <laurendobratz@icloud.com>
2021-02-18 17:05:40 +0000

    return True instead of exit statement

--- console.py
+++ console.py
@@ -28,7 +28,7 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, arg):
         """Quit command to exit the program
         """
-        sys.exit()
+        return True
 
     def do_EOF(self, arg):
         """EOF command to exit the program
