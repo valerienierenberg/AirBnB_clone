ea862aeb0d7363e6aaaeb89a952dfe8f89dc8283
valerienierenberg <valerie.nierenberg@gmail.com>
2021-02-19 22:48:32 +0000

    Add \n in tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -18,28 +18,28 @@ class TestConsole(unittest.TestCase):
         """ Tests console method """
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("create NOPE")
-            self.assertEqual(f.getvalue(), "** class doesn't exist **\n")
+            self.assertEqual(f.getvalue(), "\n** class doesn't exist **\n")
 
     def test_destroy(self):
         """ Tests console method """
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("destroy NOPE")
-            self.assertEqual(f.getvalue(), "** class doesn't exist **\n")
+            self.assertEqual(f.getvalue(), "\n** class doesn't exist **\n")
 
     def test_show(self):
         """ Tests console method """
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("show NOPE")
-            self.assertEqual(f.getvalue(), "** class doesn't exist **\n")
+            self.assertEqual(f.getvalue(), "\n** class doesn't exist **\n")
 
     def test_all(self):
         """ Tests console method """
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("all NOPE")
-            self.assertEqual(f.getvalue(), "** class doesn't exist **\n")
+            self.assertEqual(f.getvalue(), "\n** class doesn't exist **\n")
 
     def test_update(self):
         """ Tests console method """
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("update NOPE")
-            self.assertEqual(f.getvalue(), "** class doesn't exist **\n")
+            self.assertEqual(f.getvalue(), "\n** class doesn't exist **\n")
